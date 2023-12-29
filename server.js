181af1453accb32718c79a5675dd5a5fbff6b27b
collins <collinschimezieobinwa@gmail.com>
2023-12-29 09:35:06 +0100

    add CORS to API

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const booksRoute = require('./routes/books');
+const cors = require('cors');
+
 
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+// Middleware to allow cross-origin requests
+app.use(cors());
 
 app.use('/api', booksRoute);
 
