fe69d7b3ed5d1d3751f6e53103d5bd3770e0e6ab
collins <collinschimezieobinwa@gmail.com>
2024-01-18 18:01:39 +0100

    passport-jwt-auth branch

--- app.js
+++ app.js
@@ -2,42 +2,48 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
-const passport = require('passport');
-const session = require('express-session');
 const bodyParser = require('body-parser');
-const connectEnsureLogin = require('connect-ensure-login');
-const userModel = require('./models/users');
 const booksRouter = require('./routes/books');
 const cors = require('cors');
-// const authRoute = require('./routes/auth');
+const authRoute = require('./routes/auth');
 const connectDB = require('./utils/dbConnect');
+require('./authentication/auth')
 // const { render } = require('ejs');
-// require('./authentication/auth')
+// const passport = require('passport');
+// const session = require('express-session');
+// const connectEnsureLogin = require('connect-ensure-login');
+// const userModel = require('./models/users');
 
 
 // Connect to database
 connectDB();
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 3600000 }
-}));
+// Middleware to allow cross-origin requests
+app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(passport.initialize());
-app.use(passport.session());
 
-passport.use(userModel.createStrategy());
-passport.serializeUser(userModel.serializeUser());
-passport.deserializeUser(userModel.deserializeUser());
+// app.use(session({
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { maxAge: 3600000 }
+// }));
 
-app.set('views', 'views');
-app.set('view engine', 'ejs');
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-//secure the books route
-app.use('/api', connectEnsureLogin.ensureLoggedIn(), booksRouter);
+// passport.use(userModel.createStrategy());
+// passport.serializeUser(userModel.serializeUser());
+// passport.deserializeUser(userModel.deserializeUser());
+
+// app.set('views', 'views');
+// app.set('view engine', 'ejs');
+// //secure the books route
+// // app.use('/api', connectEnsureLogin.ensureLoggedIn(), booksRouter);
+
+
+app.use('/api', booksRouter);
 
 // //render the home page
 // app.get('/', (req, res) => {
@@ -55,45 +61,42 @@ app.get('/signup', (req, res) => {
 });
 
 
-//handle the signup request for new users
-app.post('/signup', (req, res) => {
-  const user = req.body;
-  console.log("USER HERE", user);
-  userModel.register(
-    new userModel({ username: user.username }), user.password, (err) => {
-      if (err) {
-        console.log('error while user register!', err);
-        res.status(500).send(err);
+// //handle the signup request for new users
+// app.post('/signup', (req, res) => {
+//   const user = req.body;
+//   console.log("USER HERE", user);
+//   userModel.register(
+//     new userModel({ username: user.username }), user.password, (err) => {
+//       if (err) {
+//         console.log('error while user register!', err);
+//         res.status(500).send(err);
 
-      } else {
-        passport.authenticate('local')(req, res, () => {
-          res.redirect('https://chimesbookstore.netlify.app/');
-        });
-      }
-    }
-  );
-});
+//       } else {
+//         passport.authenticate('local')(req, res, () => {
+//           res.redirect('https://chimesbookstore.netlify.app/');
+//         });
+//       }
+//     }
+//   );
+// });
 
-//handle the login request for existing users
-app.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), (req, res) => {
-  res.redirect('https://chimesbookstore.netlify.app/');
-});
+// //handle the login request for existing users
+// app.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), (req, res) => {
+//   res.redirect('https://chimesbookstore.netlify.app/');
+// });
 
-//handle the logout request
-app.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
-});
+// //handle the logout request
+// app.get('/logout', (req, res) => {
+//   req.logout();
+//   res.redirect('/');
+// });
 
-// Middleware to allow cross-origin requests
-app.use(cors());
 
 
 // Middleware for authentication
 // app.use('/', authRoute);
 // app.use('/api', passport.authenticate('jwt', { session: false }), booksRouter);
 
-console.log("IN SERVER NOW")
 
 // Middleware for error handling
 app.use((err, req, res, next) => {
