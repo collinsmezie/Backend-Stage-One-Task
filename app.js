0830532bb25fbbd65806aae5417cb32b7fbe3829
collins <collinschimezieobinwa@gmail.com>
2024-01-20 13:02:42 +0100

    Integrated OAuth in API

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const booksRouter = require('./routes/books');
 const cors = require('cors');
+const port = process.env.PORT || 3000;
+const { requiresAuth } = require('express-openid-connect');
 const connectDB = require('./utils/dbConnect');
-const port = process.env.PORT || 4000;
 
+const app = express();
 
-// Connect to database
+// Connect to MongoDB
 connectDB();
 
 // Middleware to allow cross-origin requests
 app.use(cors());
 
+const auth0Middleware = require('./auth/auth0');
+
 // Middleware to parse request body
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Middleware to handle routes
-app.use('/api', booksRouter);
+app.use(auth0Middleware);
 
+
+app.get('/', requiresAuth(), (req, res) => {
+  res.redirect('https://chimesbookstore.netlify.app/');
+});
+
+
+//Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
